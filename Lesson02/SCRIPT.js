3bfbd48bd7790b8f3c44f505bd9bc84df6e5bc11
Leya Leya <liliatolstyh@yandex.ru>
2019-10-21 16:20:06 +0300

    курс

---
+++ Lesson02/SCRIPT.js
@@ -0,0 +1,47 @@
+ let money;
+ money = 2000;
+ alert('money : ' + money);
+ let incom;
+ incom = 'taxi';
+ alert ('incom: ' +incom);
+ let addExpences;
+ addExpences = 'entertainment, traval, shopping' + ' ';
+ alert('addExpences: ' + addExpences);
+ let deposit;
+ deposit = true;
+ alert('deposit:  '+ deposit);
+ let mission;
+ mission = 1000000;
+ alert('mission:  '+ mission);
+ let period;
+ period = 7;
+ alert('period:  '+ period);
+
+// 2) Методы и свойства
+let money;
+money = 2000;
+console.log(typeof money);
+let income;
+income = 'taxi';
+console.log(typeof income);
+let deposit;
+deposit = true;
+console.log(typeof depoits);
+
+let string = 'income';
+console.log(string.length);
+let period;
+period = 8;
+console.log('Период '+period+ ' месяцев');
+let mission;
+mission = 1000000;
+ console.log('Цель: заработать '+mission+' долларов');
+ let str = 'addExpenses';
+ console.log(str.toLowerCase());
+ let str1 = 'a, d, d, E, x, p, e, n, s, e, s'
+ console.log(str1.split('  '));
+
+let budgetDay;
+budgetDay=4000;
+console.log('Доход за день:  '+budgetDay/30,'остаток: '+ budgetDay%30);
+
---
+++ SCRIPT.js
@@ -0,0 +1,11 @@
+// alert(1);
+// console.log(2);
+let money = 20;
+let income = 2000;
+
+const addExpenses = 'string';
+const deposit = 300;
+const mission  = {}
+const period = 2;
+alert('Good Night');
+console.log('end');
