c246a05c1e449ac37526b44d368a6e4b8b24154a
Leya Leya <liliatolstyh@yandex.ru>
2019-11-30 14:44:35 +0300

    lesson15 separated

---
+++ lesson15/3dGlo/script/TEAM.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function() {
+        'use strict';
+           
+        const  div = document.querySelectorAll('.col-md-4 col-sm-6 col-12'),
+               imgs = document.querySelectorAll('.command__photo'),
+               src ='images/command/command-';
+       imgs.forEach((img, idx) => {
+          img.addEventListener('mouseenter', (e) =>{
+         	 event.target.src = event.target.dataset.img;
+         });
+          img.addEventListener('mouseleave', (e) =>{
+         	 event.target.src = src + (idx+1) + '.jpg';                   /*"images/command/command-1.jpg"*/
+          });
+        });
+           
+         const clcTotal = document.querySelector(".calc-total");
+         clcTotal.addEventListener('input', () => {
+         	input.textContent = text.replace(/\D/g, '');
+         })
+
+});
