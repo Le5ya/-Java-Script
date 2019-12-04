ab859034a75cd3c8bebc9466928b49d637532a0b
Leya Leya <liliatolstyh@yandex.ru>
2019-12-04 16:23:48 +0300

    fetch

--- Practic/3dGlo/TIME.js
+++ Practic/3dGlo/TIME.js
@@ -344,7 +344,7 @@ const tabs = () =>{
 
                         request.open('POST','./server.php')
                         request.setRequestHeader('Content-Type', 'application/json');//или 'multipart/form-data'
-                        const formData = new RormData(form);
+                        const formData = new FormData(form);
                         let body = {};
 
                         // for (let val of formData.entries()){
@@ -359,26 +359,52 @@ const tabs = () =>{
 
                         request.send(JSON.stringify(body));
                    });
-
-
-                   
-
-
-
-
-
-
-
+            
        };
 
        sendForm();
-  
-  
-
-   
-
-
-       });//end
-
     
+           //fetch   
 
+ postData(body)
+  .then((response) => {
+    if(response.status !== 200){
+      throw new Error('status network not 200');
+    } 
+
+      statusMessage.textContent = successMessage;
+    
+    })
+    .catch((error) => {
+    statusMessage.textContent = errorMessage;
+  console.error(error); 
+    });
+  const postData = (body) => {
+    return fetch('./server.php',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:formData,
+      credentials: 'include'                             //JSON:stringify(body)
+  
+      });
+
+  return new Promis((resolve, request) =>{
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+      if(request.readyState !=== 4 ) {
+        return;
+      }
+      if (request.status === 200) {
+        resolve();
+      } else {
+        reject(request.statusText);
+      }
+      
+  });
+
+  request.open('POST', '.server.php') 
+  
+  
+    });//end
